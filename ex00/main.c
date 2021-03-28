174bbd982f6d7ed546d4892c069452593723cd4c
Matiya DERUELLE <mderuell@e3r11p4.42.fr>
2021-03-28 15:18:00 +0200

    maain et lib

--- ex00/main.c
+++ ex00/main.c
@@ -1,24 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mderuell <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/28 11:28:34 by mderuell          #+#    #+#             */
+/*   Updated: 2021/03/28 14:34:02 by mderuell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 void	ft_putstr(char *str);
-int	ft_atoi(char *str);
+int		ft_atoi(char *str);
 void	ft_putnbr(int nb);
+void	ft_output(char *arg, char *tab);
 
 int	main(int argc, char **argv)
 {
 	(void)	argc;
-	(void) 	argv;
+
 	int fd;
 	int rr;
-	char buf[692];
+	char tab[692];
+
 
 	fd = open("numbers.dict", O_RDONLY);
-	rr = read(fd, buf, 692);
-	buf[rr] = '\n';
-	ft_putstr(buf);
+	rr = read(fd, tab, 692);
+	//buf[rr] = '\n';
+	//ft_putstr(buf);
+	ft_output(argv[1],tab);
 	ft_putnbr(fd);
 	ft_putstr("\n");
 	rr = close(fd);
